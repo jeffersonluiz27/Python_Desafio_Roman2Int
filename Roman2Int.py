9afc54715f8fa6942f8c4ca74b367f7015c3c7ea
Jefferson Silva <developer.jefferson27@outlook.com>
2021-08-04 13:42:15 -0300

    Update Roman2Int.py

--- Roman2Int.py
+++ Roman2Int.py
@@ -13,8 +13,61 @@ L = 50
 C = 100
 D = 500
 M = 100
+
+
+if Algrom[-1] :
+       proxima = Numeros_dic[Algrom[t+1]]
+   else:
+       proxima = 0
 """
 
+Numeros_dic = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000    
+    }
+
+
+while True:
+    proxima=0
+    x=0
+    resultado = 0
+    print("\nConverta Algarismos Romanos em Numeros Inteiros!!")
+    acao = int(input("Para ENTRAR escolha 1 | Para SAIR escolha 0\n"))
+    if acao == 1:
+        Algrom = input("Entre com os algarismos: ")
+        AlgaUP = Algrom.upper()
+        ultimo = len(AlgaUP)
+        
+        for i in AlgaUP:
+           atual = Numeros_dic[i]
+           indice = x+1
+           
+           if indice < ultimo:
+               proxima = Numeros_dic[AlgaUP[indice]]
+               x=x+1
+           else:
+               proxima = 0
+               
+           if atual >= proxima:
+               resultado = resultado + atual
+           else:
+               resultado = resultado - atual
+               
+                
+        print("O Número correspondente é: ", resultado)
+    elif acao == 0:
+        break
+    else:
+        
+        print("\n\n\nOpção Invalida!\n\n\n");
+
+
+    
 
     
     
